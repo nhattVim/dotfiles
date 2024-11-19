f5005475b4248cf0d45d107c864c21332787d7f0
nhattVim <nhattruong13112000@gmail.com>
2024-11-19 09:16:35 +0700

    update

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,36 +1,127 @@
-# Tere
-function Invoke-Tere() {
-    $result = . (Get-Command -CommandType Application tere) $args
-    if ($result) {
-        Set-Location $result
+# Remove files or directories
+function rm
+{
+    param (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [string[]]$Path,
+
+        [switch]$Force,
+
+        [switch]$Recurse
+    )
+    foreach ($item in $Path)
+    {
+        if (-not (Test-Path -LiteralPath $item))
+        {
+            Write-Error "Path '$item' does not exist."
+            continue
+        }
+        Remove-Item -Path $item -Force:$Force.IsPresent -Recurse:$Recurse.IsPresent
     }
 }
-Set-Alias tere Invoke-Tere
 
-# Touch
-function touch {
+# Copy files or directories
+function cp
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Source,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Destination,
+
+        [switch]$Recurse
+    )
+    Copy-Item -Path $Source -Destination $Destination -Recurse:$Recurse.IsPresent
+}
+
+# Move or rename files/directories
+function mv
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Source,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Destination
+    )
+    Move-Item -Path $Source -Destination $Destination
+}
+
+# Create directories
+function mkdir
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+    New-Item -ItemType Directory -Path $Path
+}
+
+# Create file
+function touch
+{
     Param(
         [Parameter(Mandatory=$true)]
         [string]$Path
     )
-    if (Test-Path -LiteralPath $Path) {
+    if (Test-Path -LiteralPath $Path)
+    {
         (Get-Item -Path $Path).LastWriteTime = Get-Date
-    } else {
+    } else
+    {
         New-Item -Type File -Path $Path
     }
 }
 
+# Display file contents
+function cat
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+    Get-Content -Path $Path
+}
+
+
+# Where is command
+function which ($command)
+{
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Get directory
+function pwd
+{
+    Get-Location
+}
+
+# Tere
+function tere
+{
+    $result = . (Get-Command -CommandType Application tere) $args
+    if ($result)
+    {
+        Set-Location $result
+    }
+}
+
 # Neovim switcher
-function nvims() {
+function nvims()
+{
     $items = "default", "nvim-astro", "nvim-nvchad", "nvim-lazy"
     $config = $items | fzf --prompt="  Neovim Config" --height=~50% --layout=reverse --border --exit-0
 
-    if ([string]::IsNullOrEmpty($config)) {
+    if ([string]::IsNullOrEmpty($config))
+    {
         Write-Output "Nothing selected"
         break
     }
 
-    if ($config -eq "default") {
+    if ($config -eq "default")
+    {
         $config = ""
     }
 
@@ -39,66 +130,71 @@ function nvims() {
 }
 
 # Nvim AstroNvim
-function nvim-astro {
+function AstroVim
+{
     $env:NVIM_APPNAME="nvim-astro"
     nvim
 }
 
 # Nvim NvChad
-function nvim-chad {
+function NvChad
+{
     $env:NVIM_APPNAME="nvim-nvchad"
     nvim
 }
 
 # Nvim LazyVim
-function nvim-lazy {
+function LazyVim 
+{
     $env:NVIM_APPNAME="nvim-lazy"
     nvim
 }
 
-# Which
-function which ($command) {
-    Get-Command -Name $command -ErrorAction SilentlyContinue |
-        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
 # Lsd ll
-function ll {
+function ll
+{
     lsd -lh $args
 }
 
 # Lsd la
-function la {
+function la
+{
     lsd -alh $args
 }
 
 # Scoop list
-function scl {
+function scl
+{
     scoop list
 }
 
 # Scoop help
-function sch {
+function sch
+{
     scoop help
 }
 
 # Scoop cache rm *
-function scc {
+function scc
+{
     scoop cache rm *
 }
 
 # Scoop cleanup *
-function sccc {
+function sccc
+{
     scoop cleanup *
 }
 
 # Scoop update *
-function scuu {
+function scuu
+{
     scoop update *
 }
 
 # Scoop update 
-function scu {
+function scu
+{
     param (
         [string]$keyword
     )
@@ -106,7 +202,8 @@ function scu {
 }
 
 # Scoop search
-function scs {
+function scs
+{
     param (
         [string]$keyword
     )
@@ -114,7 +211,8 @@ function scs {
 }
 
 # Scoop install
-function sci {
+function sci
+{
     param (
         [string]$packageName
     )
@@ -122,7 +220,8 @@ function sci {
 }
 
 # Scoop uninstall
-function scr {
+function scr
+{
     param (
         [string]$packageName
     )
