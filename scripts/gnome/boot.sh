550ec7a6b05c709623a3a6468ff55df63984882b
nhattvim <nhattruong13112000@gmail.com>
2024-12-02 11:20:17 +0000

    update

--- scripts/gnome/boot.sh
+++ scripts/gnome/boot.sh
@@ -23,24 +23,24 @@ GUM_LINKDOWNLOADS="https://github.com/charmbracelet/gum/releases/download/v${GUM
 # update system
 echo -e "\n${NOTE} - Updating system..."
 if sudo apt update && sudo apt upgrade -y; then
-    echo -e "${OK} - System updated successfully."
+    echo -e "\n${OK} - System updated successfully."
 else
-    echo -e "${ERROR} - Failed to update the system."
+    echo -e "\n${ERROR} - Failed to update the system."
 fi
 
 # install nala
 echo -e "\n${NOTE} - Checking nala..."
 if ! command -v nala &>/dev/null; then
-    echo -e "${NOTE} - Installing and initializing nala..."
+    echo -e "\n${NOTE} - Installing and initializing nala..."
     if sudo apt install nala -y && sudo nala update && sudo nala upgrade -y; then
-        echo -e "${OK} - Nala installed successfully."
-        echo -e "${NOTE} - Fetching package lists..."
+        echo -e "\n${OK} - Nala installed successfully."
+        echo -e "\n${NOTE} - Fetching package lists..."
         sudo nala fetch
     else
-        echo -e "${ERROR} - Failed to install nala."
+        echo -e "\n${ERROR} - Failed to install nala."
     fi
 else
-    echo -e "${OK} - Nala is already installed. Skipping..."
+    echo -e "\n${OK} - Nala is already installed. Skipping..."
 fi
 
 # reload package manager
@@ -59,24 +59,24 @@ echo -e "\n${NOTE} - Installing required packages..."
 for PKG in "${pkgs[@]}"; do
     sudo $PKGMN install -y "$PKG"
     if [ $? -ne 0 ]; then
-        echo -e "${ERROR} - Failed to install $PKG, please check the script."
+        echo -e "\n${ERROR} - Failed to install $PKG, please check the script."
     else
-        echo -e "${OK} - $PKG installed successfully."
+        echo -e "\n${OK} - $PKG installed successfully."
     fi
 done
 
 # install gum (requirement)
 echo -e "\n${NOTE} - Downloading gum.deb..."
 if wget -O /tmp/gum.deb "$GUM_LINKDOWNLOADS"; then
-    echo -e "${OK} - gum.deb downloaded successfully."
-    echo -e "${NOTE} - Installing gum..."
+    echo -e "\n${OK} - gum.deb downloaded successfully."
+    echo -e "\n${NOTE} - Installing gum..."
     if sudo $PKGMN install -y /tmp/gum.deb; then
-        echo -e "${OK} - Gum installed successfully."
+        echo -e "\n${OK} - Gum installed successfully."
     else
-        echo -e "${ERROR} - Failed to install gum."
+        echo -e "\n${ERROR} - Failed to install gum."
     fi
 else
-    echo -e "${ERROR} - Failed to download gum."
+    echo -e "\n${ERROR} - Failed to download gum."
 fi
 
 clear
