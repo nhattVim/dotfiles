2e05643ec86089a3721dc86eb3b5eca71f00e0b9
nhattvim <nhattruong13112000@gmail.com>
2025-01-18 15:17:48 +0700

    WIP

--- dev/gum-style.sh
+++ dev/gum-style.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
 
-source <(curl -sSL https://is.gd/nhattVim_lib)
-
-choose "Choose your AUR helper" "yay" "paru" aur_helper
-yes_no "Do you dual boot with window?" dual_boot
-yes_no "Do you want to install GTK themes?" gtk_themes
-yes_no "Do you want to configure bluetooth?" bluetooth
-yes_no "Do you have any nvidia gpu in your system?" nvidia
-yes_no "Do you want to install Thunar file manager?" thunar
-yes_no "Do you want to install Snap (GUI packages manager)?" snapd
-yes_no "Do you want to install Homebrew (CLI package manager)?" homebrew
-yes_no "Do you want to set battery charging limit (only for laptop)?" battery
-yes_no "Install & configure SDDM log-in Manager plus (OPTIONAL) SDDM Theme?" sddm
-yes_no "Install XDG-DESKTOP-PORTAL-HYPRLAND? (For proper Screen Share ie OBS)" xdph
-yes_no "Do you want to download pre-configured Hyprland dotfiles?" dots
+source <(curl -sSL https://is.gd/nhattVim_lib) && clear
 
 gum style \
-    --border-foreground 6 --border rounded \
-    --align left --width 50 --margin "1 2" --padding "2 4" \
-    "${CYAN}Your selected options:" \
-    "${GREEN}/-/-/-/-/-/-/-/-/-/-/-/-/-/-${RESET}" \
-    "AUR Helper:${YELLOW} $aur_helper ${RESET}" \
-    "Dual Boot:${YELLOW} $dual_boot ${RESET}" \
-    "GTK Themes:${YELLOW} $gtk_themes ${RESET}" \
-    "Bluetooth:${YELLOW} $bluetooth ${RESET}" \
-    "Nvidia GPU:${YELLOW} $nvidia ${RESET}" \
-    "Thunar File Manager:${YELLOW} $thunar ${RESET}" \
-    "Snapd (GUI Packages Manager):${YELLOW} $snapd ${RESET}" \
-    "Homebrew (CLI Packages Manager):${YELLOW} $homebrew ${RESET}" \
-    "Battery Charging Limit (Laptop Only):${YELLOW} $battery ${RESET}" \
-    "SDDM Log-in Manager:${YELLOW} $sddm ${RESET}" \
-    "XDG-DESKTOP-PORTAL-HYPRLAND:${YELLOW} $xdph ${RESET}" \
-    "Download Hyprland dotfiles:${YELLOW} $dots ${RESET}" \
-    "${GREEN}\-\-\-\-\-\-\-\-\-\-\-\-\-\-${RESET}"
+    --border-foreground 212 --border rounded \
+    --align left --width 80 --margin "1 2" --padding "2 4" \
+    "${CYAN}GREAT Copy Completed." "" \
+    "${CYAN}YOU NEED to logout and re-login or reboot to avoid issues"
--- scripts/hyprland/dotfiles.sh
+++ scripts/hyprland/dotfiles.sh
@@ -47,18 +47,31 @@ fi
 
 # uncommenting WLR_NO_HARDWARE_CURSORS if nvidia is detected
 if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -iq nvidia; then
-    # NVIDIA GPU detected, uncomment line 23 in ENVariables.conf
-    sed -i '/env = WLR_NO_HARDWARE_CURSORS,1/s/^#//' config/hypr/configs/env_variables.conf
-    sed -i '/env = LIBVA_DRIVER_NAME,nvidia/s/^#//' config/hypr/configs/env_variables.conf
-    sed -i '/env = __GLX_VENDOR_LIBRARY_NAME,nvidia/s/^#//' config/hypr/configs/env_variables.conf
+    note "Nvidia GPU detected. Setting up proper env's and configs"
+    ENV_FILE="config/hypr/configs/env_variables.conf"
+    sed -i '/env = LIBVA_DRIVER_NAME,nvidia/s/^#//' "$ENV_FILE"
+    sed -i '/env = __GLX_VENDOR_LIBRARY_NAME,nvidia/s/^#//' "$ENV_FILE"
+    sed -i '/env = NVD_BACKEND,direct/s/^#//' "$ENV_FILE"
+    # enabling no hardware cursors if nvidia detected
+    sed -i 's/^\([[:space:]]*no_hardware_cursors[[:space:]]*=[[:space:]]*\)2/\1true/' config/hypr/configs/settings.conf
 fi
 
 # uncommenting WLR_RENDERER_ALLOW_SOFTWARE,1 if running in a VM is detected
 if hostnamectl | grep -q 'Chassis: vm'; then
-    note "This script is running in a virtual machine."
-    sed -i '/env = WLR_NO_HARDWARE_CURSORS,1/s/^#//' config/hypr/configs/env_variables.conf
-    sed -i '/env = WLR_RENDERER_ALLOW_SOFTWARE,1/s/^#//' config/hypr/configs/env_variables.conf
-    sed -i '/monitor = Virtual-1, 1920x1080@60,auto,1/s/^#//' config/hypr/configs/monitors.conf
+    note "System is running in a virtual machine"
+    CONFIG_FOLDER="config/hypr/configs"
+    sed -i 's/^\([[:space:]]*no_hardware_cursors[[:space:]]*=[[:space:]]*\)2/\1true/' "$CONFIG_FOLDER/settings.conf"
+    sed -i '/env = WLR_RENDERER_ALLOW_SOFTWARE,1/s/^#//' "$CONFIG_FOLDER/env_variables.conf"
+    sed -i '/monitor = Virtual-1, 1920x1080@60,auto,1/s/^#//' "$CONFIG_FOLDER/monitors.conf"
+fi
+
+# activating hyprcursor on env by checking if the directory ~/.icons/Bibata-Modern-Ice/hyprcursors exists
+if [ -d "$HOME/.icons/Bibata-Modern-Ice/hyprcursors" ]; then
+    # Define the config file path
+    HYPRCURSOR_ENV_FILE="config/hypr/configs/env_variables.conf"
+
+    sed -i 's/^#env = HYPRCURSOR_THEME,Bibata-Modern-Ice/env = HYPRCURSOR_THEME,Bibata-Modern-Ice/' "$HYPRCURSOR_ENV_FILE"
+    sed -i 's/^#env = HYPRCURSOR_SIZE,24/env = HYPRCURSOR_SIZE,24/' "$HYPRCURSOR_ENV_FILE"
 fi
 
 # Function to detect keyboard layout using localectl or setxkbmap
@@ -67,92 +80,124 @@ detect_layout() {
         layout=$(localectl status --no-pager | awk '/X11 Layout/ {print $3}')
         if [ -n "$layout" ]; then
             echo "$layout"
-        else
-            echo "unknown"
         fi
     elif command -v setxkbmap >/dev/null 2>&1; then
         layout=$(setxkbmap -query | grep layout | awk '{print $2}')
         if [ -n "$layout" ]; then
             echo "$layout"
-        else
-            echo "unknown"
         fi
-    else
-        echo "unknown"
     fi
 }
 
 # Detect the current keyboard layout
 layout=$(detect_layout)
 
+if [ "$layout" = "(unset)" ]; then
+    gum style \
+        --border-foreground 212 --border rounded \
+        --align left --width 80 --margin "1 2" --padding "2 4" \
+        "${RED}IMPORTANT WARNING${RESET}" \
+        "   - The Default Keyboard Layout could not be detected" \
+        "   - You need to set it Manually" \
+        "${YELLOW}WARNING${RESET}" \
+        "   - Setting a wrong Keyboard Layout will cause Hyprland to crash" \
+        "   - If you are not sure, just type us" \
+        "${YELLOW}NOTE:${RESET}" \
+        "   - You can also set more than 2 keyboard layouts" \
+        "   - For example us, vn, kr, gb"
+
+    act "Please enter a keyboard layout"
+    while true; do
+        new_layout=$(gum input --prompt="-> " --placeholder "Keyboard layout")
+        if [ -n "$new_layout" ]; then
+            layout="$new_layout"
+            break
+        fi
+    done
+fi
+
 note "Deteacting keyboard layout to prepare necessary changes in hyprland.conf before copying"
 
 # Prompt the user to confirm whether the detected layout is correct
-if gum confirm "Detected current keyboard layout is: $layout. Is this correct?"; then
+if gum confirm "${CYAN}Detected current keyboard layout is: ${YELLOW}'$layout'${RESET}${CYAN}.Is this correct?"; then
     # If the detected layout is correct, update the 'kb_layout=' line in the file
     awk -v layout="$layout" '/kb_layout/ {$0 = "  kb_layout=" layout} 1' config/hypr/configs/settings.conf >temp.conf
     mv temp.conf config/hypr/configs/settings.conf
     note "kb_layout $layout configured in settings."
 else
-    printf "\n%.0s" {1..2}
-    echo "$(tput bold)$(tput setaf 3)ATTENTION!!!! VERY IMPORTANT!!!! $(tput sgr0)"
-    echo "$(tput bold)$(tput setaf 7)Setting a wrong value here will result in Hyprland not starting $(tput sgr0)"
-    echo "$(tput bold)$(tput setaf 7)If you are not sure, keep it in us layout. You can change later on! $(tput sgr0)"
-    echo "$(tput bold)$(tput setaf 7)You can also set more than 2 layouts! $(tput sgr0)"
-    echo "$(tput bold)$(tput setaf 7)ie: us,vn,kr,es $(tput sgr0)"
-    printf "\n%.0s" {1..2}
-    read -p "Please enter the correct keyboard layout: " new_layout
+    gum style \
+        --border-foreground 212 --border rounded \
+        --align left --width 80 --margin "1 2" --padding "2 4" \
+        "${RED}IMPORTANT WARNING${RESET}" \
+        "   - The Default Keyboard Layout could not be detected" \
+        "   - You need to set it Manually" \
+        "${YELLOW}WARNING${RESET}" \
+        "   - Setting a wrong Keyboard Layout will cause Hyprland to crash" \
+        "   - If you are not sure, just type us" \
+        "${YELLOW}NOTE:${RESET}" \
+        "   - You can also set more than 2 keyboard layouts" \
+        "   - For example us, vn, kr, gb"
+
+    act "Please enter a keyboard layout"
+    new_layout=$(gum input --prompt="-> " --placeholder "Keyboard layout")
+
     # Update the 'kb_layout=' line with the correct layout in the file
     awk -v new_layout="$new_layout" '/kb_layout/ {$0 = "  kb_layout=" new_layout} 1' config/hypr/configs/settings.conf >temp.conf
     mv temp.conf config/hypr/configs/settings.conf
     note "kb_layout $new_layout configured in settings."
 fi
 
-# CYAN to do for better rofi appearance
-echo "${CYAN}Select monitor resolution for better Rofi appearance:"
-echo -e "\t ${YELLOW} 1. Equal to or less than 1080p (≤ 1080p)"
-echo -e "\t ${YELLOW} 2. Equal to or higher than 1440p (≥ 1440p)"
+# Check if asusctl is installed  on Startup
+if command -v asusctl >/dev/null 2>&1; then
+    sed -i '/exec-once = rog-control-center &/s/^#//' config/hypr/configs/execs.conf
+fi
+
+# Check if blueman-applet is installed and add blueman-applet on Startup
+if command -v blueman-applet >/dev/null 2>&1; then
+    sed -i '/exec-once = blueman-applet &/s/^#//' config/hypr/UserConfigs/execs.conf
+fi
+
+# Action to do for better rofi and kitty appearance
+gum style \
+    --border-foreground 212 --border rounded \
+    --align left --width 80 --margin "1 2" --padding "2 4" \
+    "${CYAN}Select monitor resolution to properly configure appearance and fonts:" "" \
+    "   - 1. for monitor res 1440p or less (< 1440p)" \
+    "   - 2. for monitors res higher than 1440p (≥ 1440p)"
 
 if gum confirm "${PINK} Enter the number of your choice: " --affirmative "≤ 1080p" --negative "≥ 1440p"; then
-    resolution="≤ 1080p"
+    resolution="< 1440p"
 else
     resolution="≥ 1440p"
 fi
 
 # Use the selected resolution in your existing script
-echo -e "\n\n${CYAN}You chose $resolution resolution for better Rofi appearance.${RESET}\n\n"
+note "You chose $resolution resolution."
 
 # Add your commands based on the resolution choice
-if [ "$resolution" == "≤ 1080p" ]; then
+if [ "$resolution" == "< 1440p" ]; then
     cp -r config/rofi/resolution/1080p/* config/rofi/
+    sed -i 's/font_size 16.0/font_size 12.0/' config/kitty/kitty.conf
+
+    # hyprlock matters
+    mv config/hypr/hyprlock.conf config/hypr/hyprlock-2k.conf
+    mv config/hypr/hyprlock-1080p.conf config/hypr/hyprlock.conf
+
 elif [ "$resolution" == "≥ 1440p" ]; then
     cp -r config/rofi/resolution/1440p/* config/rofi/
 fi
 
-### Copy Config Files ###
-set -e # Exit immediately if a command exits with a non-zero status.
+# note "By default, Rainbow Borders animation is enabled. However, this uses a bit more CPU and Memory resources."
+
+set -e
 
 note "Copying config files"
+
 folder=(
-    ranger
-    alacritty
-    btop
-    cava
-    hypr
-    kitty
-    Kvantum
-    qt5ct
-    qt6ct
-    rofi
-    swappy
-    swaync
-    swaylock
-    wal
-    waybar
-    wlogout
-    neofetch
-    Thunar
-    xfce4
+    ranger alacritty btop cava hypr
+    kitty Kvantum qt5ct qt6ct rofi swappy
+    swaync swaylock wal waybar wlogout
+    neofetch Thunar xfce4
 )
 
 for DIR in "${folder[@]}"; do
@@ -199,29 +244,36 @@ fi
 
 # additional wallpapers
 note "By default only a few wallpapers are copied..." && cd $HOME
-if gum confirm "${CAT} Would you like to download additional wallpapers?"; then
-    note "Downloading additional wallpapers..."
-    if git clone https://github.com/nhattVim/wallpapers --depth 1; then
-        note "Wallpapers downloaded successfully."
+while true; do
+    if gum confirm "${CAT} Would you like to download additional wallpapers?"; then
+        note "Downloading additional wallpapers..."
+        if git clone https://github.com/nhattVim/wallpapers --depth 1; then
+            note "Wallpapers downloaded successfully."
 
-        if cp -R wallpapers/wallpapers/* $HOME/Pictures/wallpapers/; then
-            note "Wallpapers copied successfully."
-            rm -rf wallpapers
+            if cp -R wallpapers/wallpapers/* $HOME/Pictures/wallpapers/; then
+                note "Wallpapers copied successfully."
+                rm -rf wallpapers
+                break
+            else
+                err "Copying wallpapers failed."
+            fi
         else
-            err "Copying wallpapers failed."
+            err "Downloading additional wallpapers failed"
         fi
     else
-        err "Downloading additional wallpapers failed"
+        note "You chose not to download additional wallpapers."
+        break
     fi
-else
-    note "You chose not to download additional wallpapers."
-fi
+done
 
 # symlinks for waybar style
 ln -sf "$waybar_style" "$HOME/.config/waybar/style.css" &&
 
     # initialize pywal to avoid config on hyprland
-    wal -i $wallpaper -s -t
+    # wal -i $wallpaper -s -t
+
+    # initialize wallust to avoid config error on hyprland
+    wallust run -s $wallpaper
 
 #initial symlink for Pywal Dark and Light for Rofi Themes
 ln -sf "$HOME/.cache/wal/colors-rofi-dark.rasi" "$HOME/.config/rofi/pywal-color/pywal-theme.rasi"
@@ -237,7 +289,8 @@ done
 note "Shell changed successfully to zsh."
 
 # Successfully
-printf "\n%.0s" {1..2}
-printf "\n${GREEN} Copy Completed! \n\n\n"
-printf "${CYAN} ATTENTION!!!! \n"
-printf "${CYAN} YOU NEED to logout and re-login or reboot to avoid issues\n\n"
+gum style \
+    --border-foreground 212 --border rounded \
+    --align left --width 80 --margin "1 2" --padding "2 4" \
+    "${CYAN}GREAT Copy Completed." "" \
+    "${CYAN}YOU NEED to logout and re-login or reboot to avoid issues"
