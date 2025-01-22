098195c88d6ef679a097ed0bb0b490f60af6b4f1
nhattvim <nhattruong13112000@gmail.com>
2025-01-22 22:12:02 +0700

    update

--- scripts/gnome/settings.sh
+++ scripts/gnome/settings.sh
@@ -12,20 +12,22 @@ gsettings set org.gnome.desktop.interface icon-theme "Candy-icons"
 gsettings set org.gnome.shell.extensions.user-theme name "(Modded) Catppuccin-Mocha-Standard-Mauve-Dark"
 
 note "Downloading additional wallpapers..."
-if git clone https://github.com/nhattVim/wallpapers --depth 1; then
-    note "Wallpapers downloaded successfully."
+while true; do
+    if git clone https://github.com/nhattVim/wallpapers --depth 1; then
+        note "Wallpapers downloaded successfully."
 
-    mkdir -p $HOME/Pictures/wallpapers
-    if cp -R wallpapers/wallpapers/* $HOME/Pictures/wallpapers/; then
-        note "Wallpapers copied successfully."
-        rm -rf wallpapers
-        break
+        mkdir -p $HOME/Pictures/wallpapers
+        if cp -R wallpapers/wallpapers/* $HOME/Pictures/wallpapers/; then
+            note "Wallpapers copied successfully."
+            rm -rf wallpapers
+            break
+        else
+            err "Copying wallpapers failed."
+        fi
     else
-        err "Copying wallpapers failed."
+        err "Downloading additional wallpapers failed"
     fi
-else
-    err "Downloading additional wallpapers failed"
-fi
+done
 
 gsettings set org.gnome.desktop.background picture-uri "$HOME/Pictures/wallpapers/lady-light.png"
 gsettings set org.gnome.desktop.background picture-uri-dark "$HOME/Pictures/wallpapers/anime-night.jpg"
