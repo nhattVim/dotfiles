4669593de3252dc92707a9b1edb1706c6b247c36
nhattvim <nhattruong13112000@gmail.com>
2025-01-24 20:57:52 +0700

    WIP

--- scripts/gnome/extensions.sh
+++ scripts/gnome/extensions.sh
@@ -114,7 +114,7 @@ gsettings set org.gnome.shell.extensions.logo-menu menu-button-icon-click-type 1
 gsettings set org.gnome.shell.extensions.logo-menu menu-button-terminal "kitty"
 
 # note "Configuring extension: Vitals"
-# gsettings set org.gnome.shell.extensions.vitals hot-sensors "['_memory_usage_']"
+gsettings set org.gnome.shell.extensions.vitals hot-sensors "['_memory_usage_', '__network-rx_max__']"
 
 # note "Configuring extension: forge"
 # gsettings set org.gnome.shell.extensions.forge focus-border-toggle true
