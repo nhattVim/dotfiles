dbe15d17d46ecc3bb92aed7787a1a430dcd25c3e
nhattvim <nhattruong13112000@gmail.com>
2025-01-25 20:38:29 +0700

    WIP

--- scripts/gnome/settings.sh
+++ scripts/gnome/settings.sh
@@ -22,17 +22,7 @@ gsettings set org.gnome.shell favorite-apps "@as []"
 gsettings set org.gnome.desktop.default-applications.terminal exec 'kitty'
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg ''
 
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Unikey')]"
+
 gsettings set org.gnome.login-screen banner-message-enable true
 gsettings set org.gnome.login-screen banner-message-text "Welcome to nhattVim's Linux Setup"
-
-# gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Control><Shift>b', 'XF86Keyboard']"
-# gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift><Control>', '<Shift>XF86Keyboard']"
-#
-# gsettings get org.gnome.desktop.wm.keybindings switch-input-source
-# gsettings reset org.gnome.desktop.wm.keybindings switch-input-source
-#
-# gsettings get org.gnome.desktop.wm.keybindings switch-input-source-backward
-# gsettings reset org.gnome.desktop.wm.keybindings switch-input-source-backward
-#
-# gsettings set org.freedesktop.ibus.general.hotkey triggers "['<Control><Shift>']"
-# # gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Shift><Alt>']"
