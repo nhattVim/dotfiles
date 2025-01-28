79a5c5c79f63a409b7e2938d6174460a2fe2d846
nhattvim <nhattruong13112000@gmail.com>
2025-01-28 19:56:11 +0700

    update

--- config/hypr/scripts/boot.sh
+++ config/hypr/scripts/boot.sh
@@ -5,13 +5,18 @@
 # not necessary to do since this script is only designed to run only once as long as the marker exists
 # However, I do highly suggest not to touch it since again, as long as the marker exist, script wont run
 
+# apearance
+kvantum_theme="Tokyo-Night"
+color_scheme="prefer-dark"
+gtk_theme="Tokyonight-Dark"
+icon_theme="Tokyonight-SE"
+cursor_theme="Bibata-Modern-Ice"
+
 # variables
 scriptsDir=$HOME/.config/hypr/scripts
 waybar_style="$HOME/.config/waybar/style/[Wallust] Simple.css"
 wallpaper="$HOME/Pictures/wallpapers/art-3.png"
 notif="$HOME/.config/swaync/images/bell.png"
-kvantum_theme="Tokyo-Night"
-
 swww="swww img"
 effect="--transition-bezier .43,1.19,1,.4 --transition-fps 30 --transition-type grow --transition-pos 0.925,0.977 --transition-duration 2"
 
@@ -25,10 +30,10 @@ if [ -f "$wallpaper" ]; then
 fi
 
 # initiate GTK dark mode and apply icon and cursor theme
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark >/dev/null 2>&1 &
-gsettings set org.gnome.desktop.interface gtk-theme Tokyonight-Dark >/dev/null 2>&1 &
-gsettings set org.gnome.desktop.interface icon-theme Tokyonight-SE >/dev/null 2>&1 &
-gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice >/dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface color-scheme "$color_scheme" >/dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface gtk-theme "$gtk_theme" >/dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface icon-theme "$icon_theme" >/dev/null 2>&1 &
+gsettings set org.gnome.desktop.interface cursor-theme "$cursor_theme" >/dev/null 2>&1 &
 gsettings set org.gnome.desktop.interface cursor-size 24 >/dev/null 2>&1 &
 
 # initiate kvantum theme
