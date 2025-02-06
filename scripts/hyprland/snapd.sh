04313c65b706cf68b1ff7b7040dec49f2bf9fa71
nhattvim <nhattruong13112000@gmail.com>
2025-02-06 15:10:50 +0700

    WIP

--- scripts/hyprland/snapd.sh
+++ scripts/hyprland/snapd.sh
@@ -15,25 +15,9 @@ else
 fi
 
 # Setup snapd before proceeding
-# note "Setting up snap ..."
-# sudo systemctl enable --now snapd.socket || {
-#     err "Failed to enable snapd"
-# }
-
-# Create /snap symlink
-# if [ ! -e /snap ]; then
-#     sudo ln -s /var/lib/snapd/snap /snap || {
-#         err "Failed to create /snap symlink"
-#     }
-# fi
-
-# Check for AppArmor
-# if systemctl list-units --all --type=service | grep -q "snapd.apparmor"; then
-#     sudo systemctl enable --now snapd.apparmor || {
-#         err "Failed to enable snapd.apparmor"
-#     }
-# else
-#     note "snapd.apparmor not available on this system."
-# fi
-
-ok "Snap setup completed successfully!"
+act "Enabling snap..."
+if sudo systemctl enable --now snapd; then
+    ok "Snap setup completed successfully!"
+else
+    err "Failed to enable snapd"
+fi
