190c3507b44f14bd49c5c612da7acd8ec777885f
Lê Nhật Trường <126251176+nhattVim@users.noreply.github.com>
2025-02-11 18:32:32 +0700

    WIP

--- scripts/hyprland/snapd.sh
+++ scripts/hyprland/snapd.sh
@@ -23,7 +23,7 @@ else
 fi
 
 act "Installing snap-store..."
-if snap install snap-store && snap install snapd-desktop-integration; then
+if sudo snap install snap-store && sudo snap install snapd-desktop-integration; then
     ok "Install snap-store successfully"
 else
     err "Failed to install snap-store"
