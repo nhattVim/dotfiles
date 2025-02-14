8dcff99c805c4125a25b8e564feb5afd366e82c0
nhattvim <nhattruong13112000@gmail.com>
2025-02-14 21:58:26 +0700

    update

--- config/hypr/scripts/night_mode.sh
+++ config/hypr/scripts/night_mode.sh
@@ -1,33 +1,38 @@
 #!/bin/sh
 
-# Mark toggle
+# File to mark toggle state
 TOGGLE=$HOME/.cache/.gammastep_toggle
 
-# Location
+# Location (Quy Nhon, Binh Dinh, Vietnam)
 LAT=13.782
 LON=109.219
+
+# Night light color temperature
+TEMP_NIGHT=3500
 
 # Get flags
 while getopts t flag; do
     case "${flag}" in
-    t) toggle=1 ;;
+    t) toggle=1 ;; # If -t is set, enable toggle mode
     esac
 done
 
-# flag -t : toggle on/off
+# If -t is set, toggle on/off
 if [ "$toggle" = "1" ]; then
+    # If toggle file does not exist, turn on night light
     if [ ! -e $TOGGLE ]; then
         touch $TOGGLE
-        gammastep -l $LAT:$LON -m wayland &
+        gammastep -l $LAT:$LON -O $TEMP_NIGHT -m wayland &
     else
+        # If toggle file exists, turn off night light
         rm $TOGGLE
         pkill gammastep
     fi
     exit 0
 fi
 
-# flag -t not set
+# If -t is not set, check state and run in background
 if [ ! -e $TOGGLE ]; then
     touch $TOGGLE
-    gammastep -l $LAT:$LON -m wayland &
+    gammastep -l $LAT:$LON -O $TEMP_NIGHT -m wayland &
 fi
