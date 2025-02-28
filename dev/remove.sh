a549e9a97392d68c9127c003182c3be316da94c3
nhattvim <nhattruong13112000@gmail.com>
2025-02-28 08:18:31 +0700

    WIP

---
+++ dev/remove.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# sudo pacman -Rns code
+# yay -Rns visual-studio-code-bin
+# yay -Rns vscodium-bin
+
+rm -rf ~/.config/Code
+rm -rf ~/.vscode
+rm -rf ~/.cache/Code
+rm -rf ~/.local/share/Code
+
+sudo rm -rf /usr/lib/code
+sudo rm -rf /usr/share/code
+
+rm -rf ~/.vscode-oss
+rm -rf ~/.config/VSCodium
+rm -rf ~/.local/share/vscode-oss
+
+sudo rm -rf /var/lib/code
--- scripts/hyprland/vscode.sh
+++ scripts/hyprland/vscode.sh
@@ -12,25 +12,34 @@ case "$vscode" in
 "code")
     note "Installing VSCode OSS..."
     iPac code
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     ;;
 "visual-studio-code-bin")
     note "Installing Visual Studio Code (bin)..."
     iAur visual-studio-code-bin
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     ;;
 "vscodium")
     note "Installing VSCodium..."
     iAur vscodium-bin
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
     ;;
 esac
 
 # Clone dotfiles
 temp=$(mktemp -d)
-git clone -b hyprland https://github.com/nhattVim/dotfiles.git --depth 1 "$temp" || exit 1
+git clone https://github.com/nhattVim/dotfiles.git --depth 1 "$temp" || exit 1
 
 # Detect vscode config directory
 case "$vscode" in
-"code") vscode_config="$HOME/.config/Code" ;;
-"visual-studio-code-bin") vscode_config="$HOME/.config/Code - OSS" ;;
+    "code") vscode_config="$HOME/.config/Code - OSS" ;;
+"visual-studio-code-bin") vscode_config="$HOME/.config/Code" ;;
 "vscodium") vscode_config="$HOME/.config/VSCodium" ;;
 esac
 
@@ -39,14 +48,23 @@ mkdir -p "$vscode_config/User"
 
 # Copy settings.json, keybindings.json from dotfiles
 act "Copying VSCode settings..."
-cp -f "$temp/assest/vscode/settings.json" "$vscode_config/User/settings.json"
-cp -f "$temp/assest/vscode/keybindings.json" "$vscode_config/User/keybindings.json"
+cp -f "$temp/assets/vscode/settings.json" "$vscode_config/User/settings.json"
+cp -f "$temp/assets/vscode/keybindings.json" "$vscode_config/User/keybindings.json"
 
 # Install extensions
 act "Installing extensions..."
 while read -r extension; do
     "$vscode" --install-extension "$extension"
-done <"$temp/assest/vscode/extensions.txt"
+done <"$temp/assets/vscode/extensions.txt"
+
+cat "$temp/assets/vscode/extensions.txt" | xargs -I {} code --install-extension {}
+
+# Set ownership
+case "$vscode" in
+"code") sudo chown -R $(whoami) '/usr/lib/code' ;;
+"visual-studio-code-bin") ;;
+"vscodium") ;;
+esac
 
 # Done
 ok "VSCode setup completed successfully!"
