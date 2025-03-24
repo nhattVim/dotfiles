8b4545cdc96e75e1a3131df4553abb129599c948
nhattVim <nhattruong13112000@gmail.com>
2025-03-24 20:28:00 +0700

    WIP

--- scripts/gnome/pkgs.sh
+++ scripts/gnome/pkgs.sh
@@ -76,6 +76,9 @@ done
 if ! command -v nodejs &>/dev/null; then
     note "Install nvm (node version manager) ..."
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
     note "Install lastest version of Node.js ..."
     if nvm install node; then
         ok "Node.js has been successfully installed."
