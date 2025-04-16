48396b7b044c9a9629990e6cd936f9270359596d
nhattVim <nhattruong13112000@gmail.com>
2025-04-16 20:45:29 +0700

    WIP

--- assets/vscode/custom.js
+++ assets/vscode/custom.js
@@ -6,95 +6,44 @@ document.addEventListener('DOMContentLoaded', function () {
     const checkElement = setInterval(() => {
         const commandDialog = document.querySelector(".quick-input-widget");
         if (commandDialog) {
-            // Apply the blur effect immediately if the command dialog is visible
+            // Check if the command palette element is visible for the first time.
             if (commandDialog.style.display !== "none") {
-                runMyScript();
+                applyBlur();
             }
-            // Create an DOM observer to 'listen' for changes in element's attribute.
-            const observer = new MutationObserver((mutations) => {
-                mutations.forEach((mutation) => {
-                    if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-                        if (commandDialog.style.display === 'none') {
-                            handleEscape();
-                        } else {
-                            // If the .quick-input-widget element (command palette) is in the DOM
-                            // but no inline style display: none, show the backdrop blur.
-                            runMyScript();
-                        }
-                    }
-                });
-            });
-
-            observer.observe(commandDialog, { attributes: true });
-
-            // Clear the interval once the observer is set
+            observeCommandDialog(commandDialog);
             clearInterval(checkElement);
-        } else {
-            console.log("Command dialog not found yet. Retrying...");
         }
-    }, 500); // Check every 500ms
+    }, 500); // Check every 0.5s
 
-    // Execute when command palette was launched.
-    document.addEventListener('keydown', function (event) {
-        if ((event.metaKey || event.ctrlKey) && event.key === 'p') {
-            event.preventDefault();
-            runMyScript();
-        } else if (event.key === 'Escape' || event.key === 'Esc') {
-            event.preventDefault();
-            handleEscape();
-        }
-    });
-
-    // Ensure the escape key event listener is at the document level
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape' || event.key === 'Esc') {
-            handleEscape();
-        }
-    }, true);
-
-    function runMyScript() {
-        const targetDiv = document.querySelector(".monaco-workbench");
-
-        // Remove existing element if it already exists
-        const existingElement = document.getElementById("bg-blur");
-        existingElement && existingElement.remove();
-
-        // Create and configure the new element
-        const newElement = document.createElement("div");
-        newElement.setAttribute('id', 'bg-blur');
-
-        newElement.addEventListener('click', function () {
-            newElement.remove();
+    function observeCommandDialog(commandDialog) {
+        const observer = new MutationObserver(() => {
+            if (commandDialog.style.display !== "none") {
+                applyBlur();
+            } else {
+                removeBlur();
+            }
         });
 
-        // Append the new element as a child of the targetDiv
-        targetDiv.appendChild(newElement);
-
-        // Hide the sticky widget
-        const widgets = document.querySelectorAll(".sticky-widget");
-        widgets.forEach((widget) => {
-            widget.style.opacity = 0;
-        });
-
-        // Hide the tree sticky widget
-        const treeWidget = document.querySelector(".monaco-tree-sticky-container");
-        treeWidget && (treeWidget.style.opacity = 0);
+        observer.observe(commandDialog, { attributes: true });
     }
 
-    // Remove the backdrop blur from the DOM when esc key is pressed.
-    function handleEscape() {
-        const element = document.getElementById("bg-blur");
-        element && element.click();
+    function applyBlur() {
+        const targetDiv = document.querySelector(".monaco-workbench");
+        let blurElement = document.getElementById("bg-blur");
 
-        // Show the sticky widget
-        const widgets = document.querySelectorAll(".sticky-widget");
-        widgets.forEach((widget) => {
-            widget.style.opacity = 1;
-        });
+        if (!blurElement) {
+            blurElement = document.createElement("div");
+            blurElement.setAttribute('id', 'bg-blur');
+            blurElement.addEventListener('click', removeBlur);
+            targetDiv.appendChild(blurElement);
+        }
+    }
 
-        // Show the tree sticky widget
-        const treeWidget = document.querySelector(".monaco-tree-sticky-container");
-        treeWidget && (treeWidget.style.opacity = 1);
+    function removeBlur() {
+        const blurElement = document.getElementById("bg-blur");
+        if (blurElement) {
+            blurElement.remove();
+        }
     }
 });
 
