e05b51452364355448b4eb41d6074b504e5a2b2e
nhattVim <nhattruong13112000@gmail.com>
2025-04-16 19:49:57 +0700

    WIP

--- assets/vscode/custom.js
+++ assets/vscode/custom.js
@@ -1,3 +1,7 @@
+// * ==============================================================================
+// * Cursor Effect (like neovide)
+// * ==============================================================================
+
 // https://www.reddit.com/r/vscode/comments/11e66xh/i_made_neovide_alike_cursor_effect_on_vscode/
 
 // Configuration
@@ -360,3 +364,52 @@ createCursorHandler({
     },
 
 })
+
+// * ==============================================================================
+// * Commad Palette Blur Effect
+// * ==============================================================================
+
+document.addEventListener('DOMContentLoaded', function () {
+    const checkElement = setInterval(() => {
+        const commandDialog = document.querySelector(".quick-input-widget");
+        if (commandDialog) {
+            // Check if the command palette element is visible for the first time.
+            if (commandDialog.style.display !== "none") {
+                applyBlur();
+            }
+            observeCommandDialog(commandDialog);
+            clearInterval(checkElement);
+        }
+    }, 500); // Check every 0.5s
+
+    function observeCommandDialog(commandDialog) {
+        const observer = new MutationObserver(() => {
+            if (commandDialog.style.display !== "none") {
+                applyBlur();
+            } else {
+                removeBlur();
+            }
+        });
+
+        observer.observe(commandDialog, { attributes: true });
+    }
+
+    function applyBlur() {
+        const targetDiv = document.querySelector(".monaco-workbench .part.editor>.content");
+        let blurElement = document.getElementById("bg-blur");
+
+        if (!blurElement) {
+            blurElement = document.createElement("div");
+            blurElement.setAttribute('id', 'bg-blur');
+            blurElement.addEventListener('click', removeBlur);
+            targetDiv.appendChild(blurElement);
+        }
+    }
+
+    function removeBlur() {
+        const blurElement = document.getElementById("bg-blur");
+        if (blurElement) {
+            blurElement.remove();
+        }
+    }
+});
