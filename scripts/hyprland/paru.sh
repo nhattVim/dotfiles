e44c30465ddf1e58c3803931775ce86365efce76
Lê Nhật Trường <126251176+nhattVim@users.noreply.github.com>
2025-07-09 10:00:58 +0700

    Update paru.sh

--- scripts/hyprland/paru.sh
+++ scripts/hyprland/paru.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Paru AUR Helper
+# Paru AUR Helper Installer
 
 # Source library
 . <(curl -sSL https://raw.githubusercontent.com/nhattVim/dotfiles/refs/heads/master/scripts/lib.sh)
+
+ISAUR=$(command -v yay || command -v paru)
 
 if [ -n "$ISAUR" ]; then
     ok "AUR helper already installed, moving on."
@@ -13,36 +15,36 @@ else
     attempt=1
 
     while [ $attempt -le $MAX_RETRIES ]; do
-        note "Installing paru from (Attempt $attempt) AUR..."
+        note "Installing paru from AUR (Attempt $attempt)..."
 
         temp_dir=$(mktemp -d)
 
-        git clone https://aur.archlinux.org/paru-bin.git "$temp_dir" && cd "$temp_dir" || {
-            err "Failed to clone paru from AUR (Attempt $attempt)"
-            ((attempt++))
-            continue
-        }
-
-        if makepkg -si --noconfirm; then
-            ok "Successfully installed paru!"
-            rm -rf "$temp_dir"
-            break
+        if git clone https://aur.archlinux.org/paru-bin.git "$temp_dir" && cd "$temp_dir"; then
+            if makepkg -si --noconfirm; then
+                ok "Successfully installed paru!"
+                rm -rf "$temp_dir"
+                break
+            else
+                err "Failed to install paru (Attempt $attempt)"
+            fi
         else
-            err "Failed to install paru from AUR (Attempt $attempt)"
-            rm -rf "$temp_dir"
-            ((attempt++))
+            err "Failed to clone paru (Attempt $attempt)"
         fi
 
-        if [ $attempt -gt $MAX_RETRIES ]; then
-            err "Exceeded maximum retries. Exiting..."
-            exit 1
-        fi
+        rm -rf "$temp_dir"
+        ((attempt++))
     done
+
+    if [ $attempt -gt $MAX_RETRIES ]; then
+        err "Exceeded maximum retries. Exiting..."
+        exit 1
+    fi
+
+    ISAUR=$(command -v yay || command -v paru)
 fi
 
 # Update system before proceeding
 note "Performing a full system update to avoid issues..."
-ISAUR=$(command -v yay || command -v paru)
 
 $ISAUR -Syu --noconfirm || {
     err "Failed to update system"
